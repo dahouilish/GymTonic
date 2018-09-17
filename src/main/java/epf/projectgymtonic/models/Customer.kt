92e185ddb5237dbeb60419ca79ab43dfd6c922bf
ldeguerr <lancelot.deguerre@epfedu.fr>
2018-09-17 14:24:43 +0200

    [LDE] test

--- src/main/java/epf/projectgymtonic/models/Customer.kt
+++ src/main/java/epf/projectgymtonic/models/Customer.kt
@@ -9,6 +9,7 @@ import javax.persistence.Id
  * La "data class" représente un "POJO" (plain-old java object), c'est à dire un objet qui n'est destiné qu'à "représenter" une entité.
  * Ici, en l'occurence, un utilisateur.
  * @author Loïc Ortola on 10/09/2018
+ * Test Lancelot
  */
 @Entity
 data class Customer(@Id @GeneratedValue var id: Int? =  null, var firstName: String? = null, var lastName: String?= null, var password: String?= null, var mail: String?= null) {
